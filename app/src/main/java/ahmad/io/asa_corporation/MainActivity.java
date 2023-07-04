bcc8e19113c35b6951da5ba25ebb16caad432c7e
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-07-04 14:01:48 +0200

    project setup

---
+++ app/src/main/java/ahmad/io/asa_corporation/MainActivity.java
@@ -0,0 +1,14 @@
+package ahmad.io.asa_corporation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
