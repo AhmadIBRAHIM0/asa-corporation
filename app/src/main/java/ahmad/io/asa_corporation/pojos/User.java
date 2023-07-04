613a6743164ef30ca733144857cba524d23f59fa
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-07-04 14:50:24 +0200

    create the user model

---
+++ app/src/main/java/ahmad/io/asa_corporation/pojos/User.java
@@ -0,0 +1,27 @@
+package ahmad.io.asa_corporation.pojos;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    @ColumnInfo(name = "password")
+    private String password;
+
+    @ColumnInfo(name = "phone")
+    private String phone;
+
+    @ColumnInfo(name = "role")
+    private String role;
+}
