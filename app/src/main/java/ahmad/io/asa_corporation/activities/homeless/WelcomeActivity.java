97a8c13baccfe7902aaafaa757d2225a440955c7
Ahmad IBRAHIM <ahmadji.ibr11@gmail.com>
2023-07-06 10:32:55 +0200

    create the welcome page activity

---
+++ app/src/main/java/ahmad/io/asa_corporation/activities/homeless/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package ahmad.io.asa_corporation.activities.homeless;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ahmad.io.asa_corporation.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
